f81fc4e4d449d4aea3d70e9cf449bc70659f4c62
blue_lobster <vincent.pu@outlook.com>
2022-09-13 16:29:58 -0400

    remove I peremeter

--- lineTrack/src/main.cpp
+++ lineTrack/src/main.cpp
@@ -5,14 +5,16 @@ LineTrack* Track;
 void setup() {
   Serial.begin(9600);
   chassis.init();
-  Track = new LineTrack(500,1.5,2,0);
+  Track = new LineTrack(500,1.5,0,2);
 }
 bool atCross = false;
 void loop() {
   atCross = Track->isCross();
   if(atCross){
     chassis.setWheelSpeeds(0,0);
+    chassis.driveFor(7.62,10);
+    
   }else{
-    Track->track(5);
+    Track->track(10);
   }
 }
